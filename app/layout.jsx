2fadb9e10e6c0013a1a5beb7aa4844554d86a6e9
iykay1234 <uzodinmaikechukwu162@gmail.com>
2024-11-12 21:05:36 +0100

    initial project

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css'
+import { Children } from 'react'
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover and Share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+  return (
+   <html lang='en'>
+    <body>
+        <div className='main'>
+            <div className='gradient'/>
+
+        </div>
+        <main className='app'>
+            {children}
+
+        </main>
+    </body>
+   </html>
+  )
+}
+
+export default RootLayout;
---
+++ components/Provider.jsx
@@ -0,0 +1,9 @@
+import Feed from '@/components/Feed'
+
+const Provider = () => {
+  return (
+    <div>Provider</div>
+  )
+}
+
+export default Provider
